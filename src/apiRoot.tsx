5ae11beac8041c0664cb8d44bbd7de0192a8e2fd
Ryan Huddleston <wryanhuddleston@gmail.com>
2022-01-06 13:50:20 +0900

    added apiRoot to rails heroku BE

--- src/apiRoot.tsx
+++ src/apiRoot.tsx
@@ -1,4 +1,4 @@
-const ROOT_URL="http://localhost:3001/"
-// const ROOT_URL=""
+// const ROOT_URL="http://localhost:3001/"
+const ROOT_URL="https://medi-app-be.herokuapp.com/"
 
 export { ROOT_URL };
